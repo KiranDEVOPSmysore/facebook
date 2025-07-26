e17cbcd5decb56b471c5845e476822536075fc37
Dee <deepak@gmail.com>
2025-07-26 17:46:47 +0530

    testing for remote

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 This line is added in master branch for the second time
 This changes are made in UAT
-This is master branch
+This is master
+Updated the line for testing
+branch
 This changes are made in dev branch
 This is deployment file
 updating this line
